6de0aeadcf6ed91c0f757f252ab6702b52521e07
Micael Trevisan <micaelxv1@gmail.com>
2024-06-05 23:47:24 -0300

    Loja

--- pages/index.vue
+++ pages/index.vue
@@ -5,27 +5,27 @@
         <div>
           <v-hover v-slot="{ isHovering, props }">
             <v-card
-              class="ma-3"
+              class="ma-3 ml-12"
               width="370"
               height="350"
               v-bind="props"
               elevation="2"
               style="border-radius: 25px"
-              >
+            >
               <v-img
-              v-if="item"
+                v-if="item"
                 class="mx-auto"
                 :src="item.image"
                 :aspect-ratio="4 / 4"
-                max-width="200"
-                max-height="240"
+                max-width="210"
+                max-height="210"
                 cover
-                >
+              >
                 <v-expand-transition>
                   <div
                     v-if="isHovering"
                     class="d-flex transition-fast-in-fast-out bg-orange-darken-2 v-card--reveal text-h2"
-                    style="height: 100%;"
+                    style="height: 100%"
                   >
                     <div v-if="item">
                       <div
@@ -45,12 +45,57 @@
                 >
                   {{ item.name }}
                 </h3>
+                <div class="font-weight-light text-h6 mb-2">
+                  {{ item.description }}
+                </div>
               </v-card-text>
             </v-card>
           </v-hover>
         </div>
       </v-col>
     </v-row>
+    <v-footer
+    class="bg-indigo-lighten-1 text-center d-flex flex-column"
+  >
+    <div>
+      <v-btn
+        icon="mdi-facebook"
+        href="https://www.facebook.com/"
+        target="_blank"
+        class="mx-4"
+        variant="text"
+      />
+      <v-btn
+        icon="mdi-github"
+        href="https://github.com/"
+        target="_blank"
+        class="mx-4"
+        variant="text"
+      />
+      <v-btn
+        icon="mdi-twitter"
+        href="https://www.x.com/"
+        target="_blank"
+        class="mx-4"
+        variant="text"
+      />
+      <v-btn
+        icon="mdi-instagram"
+        href="https://www.instagram.com/"
+        target="_blank"
+        class="mx-4"
+        variant="text"
+      />
+    </div>
+
+    <div class="pt-0">
+      Seja para a sua lista de compras diárias ou para aqueles itens especiais que você deseja encontrar, o nosso mercado tem de tudo! De produtos frescos e orgânicos a eletrônicos de última geração, e de produtos de cuidados pessoais a utensílios domésticos, nós temos tudo o que você precisa em um só lugar. Com uma ampla variedade de marcas e preços competitivos, estamos aqui para atender a todas as suas necessidades de compras. Visite-nos e descubra a conveniência de encontrar tudo o que procura em um único destino!    </div>
+    <v-divider/>
+
+    <div>
+      {{ new Date().getFullYear() }} — <strong>CRSTORE</strong>
+    </div>
+  </v-footer>
   </body>
 </template>
 
