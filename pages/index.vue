c9942b1af9f2fb512d51f300eee9f02f16bf601c
Micael Trevisan <micaelxv1@gmail.com>
2024-06-06 00:14:13 -0300

    Loja

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
             <v-card
               class="ma-3 ml-12"
               width="370"
-              height="350"
+              height="380"
               v-bind="props"
               elevation="2"
               style="border-radius: 25px"
@@ -32,70 +32,75 @@
                         class="font-weight-light text-h6 mb-2"
                         style="justify-content: center"
                       >
-                        {{ item.price }}
+                        ${{ item.price }}
                       </div>
                     </div>
                   </div>
                 </v-expand-transition>
               </v-img>
               <v-card-text class="pt-6">
+                <v-btn prepend-icon="mdi-cart-outline" variant="outlined" >Comprar</v-btn>
                 <h3
                   v-if="item"
                   class="text-h4 font-weight-light text-orange mb-2"
                 >
                   {{ item.name }}
-                </h3>
-                <div class="font-weight-light text-h6 mb-2">
-                  {{ item.description }}
-                </div>
+                  </h3>
+                  <div class="font-weight-light text-h6 mb-2">
+                    {{ item.description }}
+                    </div>
               </v-card-text>
             </v-card>
           </v-hover>
         </div>
       </v-col>
     </v-row>
-    <v-footer
-    class="bg-indigo-lighten-1 text-center d-flex flex-column"
-  >
-    <div>
-      <v-btn
-        icon="mdi-facebook"
-        href="https://www.facebook.com/"
-        target="_blank"
-        class="mx-4"
-        variant="text"
-      />
-      <v-btn
-        icon="mdi-github"
-        href="https://github.com/"
-        target="_blank"
-        class="mx-4"
-        variant="text"
-      />
-      <v-btn
-        icon="mdi-twitter"
-        href="https://www.x.com/"
-        target="_blank"
-        class="mx-4"
-        variant="text"
-      />
-      <v-btn
-        icon="mdi-instagram"
-        href="https://www.instagram.com/"
-        target="_blank"
-        class="mx-4"
-        variant="text"
-      />
-    </div>
+    <v-footer class="bg-indigo-lighten-1 text-center d-flex flex-column">
+      <div>
+        <v-btn
+          icon="mdi-facebook"
+          href="https://www.facebook.com/"
+          target="_blank"
+          class="mx-4"
+          variant="text"
+        />
+        <v-btn
+          icon="mdi-github"
+          href="https://github.com/"
+          target="_blank"
+          class="mx-4"
+          variant="text"
+        />
+        <v-btn
+          icon="mdi-twitter"
+          href="https://www.x.com/"
+          target="_blank"
+          class="mx-4"
+          variant="text"
+        />
+        <v-btn
+          icon="mdi-instagram"
+          href="https://www.instagram.com/"
+          target="_blank"
+          class="mx-4"
+          variant="text"
+        />
+      </div>
 
-    <div class="pt-0">
-      Seja para a sua lista de compras diárias ou para aqueles itens especiais que você deseja encontrar, o nosso mercado tem de tudo! De produtos frescos e orgânicos a eletrônicos de última geração, e de produtos de cuidados pessoais a utensílios domésticos, nós temos tudo o que você precisa em um só lugar. Com uma ampla variedade de marcas e preços competitivos, estamos aqui para atender a todas as suas necessidades de compras. Visite-nos e descubra a conveniência de encontrar tudo o que procura em um único destino!    </div>
-    <v-divider/>
+      <div class="pt-0">
+        Seja para a sua lista de compras diárias ou para aqueles itens especiais
+        que você deseja encontrar, o nosso mercado tem de tudo! De produtos
+        frescos e orgânicos a eletrônicos de última geração, e de produtos de
+        cuidados pessoais a utensílios domésticos, nós temos tudo o que você
+        precisa em um só lugar. Com uma ampla variedade de marcas e preços
+        competitivos, estamos aqui para atender a todas as suas necessidades de
+        compras. Visite-nos e descubra a conveniência de encontrar tudo o que
+        procura em um único destino!
+      </div>
+      <v-divider />
 
-    <div>
-      {{ new Date().getFullYear() }} — <strong>CRSTORE</strong>
-    </div>
-  </v-footer>
+      <div>{{ new Date().getFullYear() }} — <strong>CRSTORE</strong></div>
+    </v-footer>
   </body>
 </template>
 
